209eb0349f68cf5b0ebea2bfa0585e99da4a4ba3
Yonathan Rizky <yonathan.nathanr@gmail.com>
2022-12-20 10:18:01 +0700

    refactor code & validation

--- fastify/controllers/login.js
+++ fastify/controllers/login.js
@@ -1,9 +1,18 @@
 const bcrypt = require("bcrypt");
 const { JWT_SECRET } = process.env;
 const jwt = require("jsonwebtoken");
+const Ajv = require("ajv");
+const { responseFormatter } = require("../utils");
+const ajv = new Ajv();
+
 const index = async (request, reply) => {
+  const validate = await validateRequest(request, reply);
+
+  if (validate.message) {
+    return reply.code(400).send(responseFormatter(400, null, validate));
+  }
+
   const body = request.body;
-  const name = body.name;
   const email = body.email;
   let password = body.password;
 
@@ -11,9 +20,23 @@ const index = async (request, reply) => {
     email,
   });
   user = user[0];
+  if (user === undefined) {
+    return reply
+      .code(404)
+      .send(responseFormatter(404, null, { message: "User not found" }));
+  }
+
   let userPassword = user.password;
+  const name = user.name;
 
   const isValidPassword = await bcrypt.compare(password, userPassword);
+  if (!isValidPassword) {
+    return reply.code(401).send(
+      responseFormatter(401, null, {
+        message: "Password Wrong, Please try again",
+      })
+    );
+  }
 
   const data = {
     name,
@@ -24,12 +47,47 @@ const index = async (request, reply) => {
     expiresIn: 86400,
   });
 
-  return reply.send({
-    name,
-    email,
-    key_token: "Bearer",
-    token,
+  return reply.send(
+    responseFormatter(200, "Success", {
+      name,
+      email,
+      key_token: "Bearer",
+      token,
+    })
+  );
+};
+
+const validateRequest = async (req, res) => {
+  const body = req.body;
+  if (!body) {
+    return {
+      message: "No request sent",
+    };
+  }
+
+  let schema = {
+    type: "object",
+    properties: {
+      email: { type: "string" },
+      password: { type: "string", minLength: 5 },
+    },
+    required: ["email", "password"],
+  };
+
+  const validate = ajv.compile(schema);
+
+  const valid = validate({
+    email: req.body.email,
+    password: req.body.password,
   });
+
+  if (!valid) {
+    return {
+      message: validate.errors[0].message,
+    };
+  }
+
+  return { error: false };
 };
 
 module.exports = {
--- fastify/controllers/users.js
+++ fastify/controllers/users.js
@@ -1,5 +1,16 @@
 const bcrypt = require("bcrypt");
+const { responseFormatter } = require("../utils");
+const { insertData } = require("../utils/queryBuilder");
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
 const create = async (request, reply) => {
+  const validate = await validateRequest(request, reply);
+
+  if (validate.message) {
+    return reply.code(400).send(responseFormatter(400, null, validate));
+  }
+
   const body = request.body;
   const name = body.name;
   const email = body.email;
@@ -11,9 +22,47 @@ const create = async (request, reply) => {
     email,
     password,
   };
-  const insert = await knex("users").insert(data);
 
-  return reply.send(data);
+  const insert = await insertData("users", data);
+  if (insert.error) {
+    return reply.code(400).send(responseFormatter(400, null, insert));
+  }
+  return reply.code(200).send(responseFormatter(200, "Success", insert));
+};
+
+const validateRequest = async (req, res) => {
+  const body = req.body;
+  if (!body) {
+    return {
+      message: "No request sent",
+    };
+  }
+
+  let schema = {
+    type: "object",
+    properties: {
+      name: { type: "string", minLength: 3 },
+      email: { type: "string" },
+      password: { type: "string", minLength: 5 },
+    },
+    required: ["name", "email", "password"],
+  };
+
+  const validate = ajv.compile(schema);
+
+  const valid = validate({
+    name: body.name,
+    email: body.email,
+    password: body.password,
+  });
+
+  if (!valid) {
+    return {
+      message: validate.errors[0].message,
+    };
+  }
+
+  return { error: false };
 };
 
 module.exports = {
